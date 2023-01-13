f2a3e9c0d89255934a5bbeac776066bcf87794bb
Rafael Belchior <rbelchior@blockdaemon.com>
2023-01-13 02:21:29 +0200

    refactor: copy rlp reader to contract folder
    Signed-off-by: Rafael Belchior <rbelchior@blockdaemon.com>

--- contracts/ProxyContract.sol
+++ contracts/ProxyContract.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.6.0 <0.8.0;
 import "./RelayContract.sol";
 import "./GetProofLib.sol";
 import "./RLPWriter.sol";
-import "solidity-rlp/contracts/RLPReader.sol";
+import "./RLPReader.sol";
 
 contract ProxyContract {
     enum NodeType { BRANCH, EXTENSION, LEAF, DELETED, HASHED }
