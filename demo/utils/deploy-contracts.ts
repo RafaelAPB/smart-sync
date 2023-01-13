ad59cac3c57541a7cf2aaa120b9f217ef61cbe3b
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2023-01-13 15:13:53 +0200

    feat: smart contract deployment script
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt´>

---
+++ demo/utils/deploy-contracts.ts
@@ -0,0 +1,72 @@
+import { ethers } from 'ethers';
+import { logger } from '../../src/utils/logger';
+
+require('dotenv').config();
+
+logger.setSettings({ minLevel: 'info', name: 'demo-utils' });
+
+const {
+    PRIVATE_KEY, RPC_URL_GOERLI, RPC_URL_MUMBAI, RPC_LOCALHOST_ORIGIN, RPC_LOCALHOST_TARGET,
+} = process.env;
+
+const goerliProvider = new ethers.providers.JsonRpcProvider(RPC_URL_GOERLI);
+const polygonProvider = new ethers.providers.JsonRpcProvider(RPC_URL_MUMBAI);
+
+const goerliSigner = new ethers.Wallet(PRIVATE_KEY as string, goerliProvider);
+const polygonSigner = new ethers.Wallet(PRIVATE_KEY as string, polygonProvider);
+
+const abiSimpleStorage = require('../../artifacts/contracts/SimpleStorage.sol/SimpleStorage.json').abi;
+const bytecodeSimpleStorage = require('../../artifacts/contracts/SimpleStorage.sol/SimpleStorage.json').bytecode;
+
+const SimpleStorage = new ethers.ContractFactory(abiSimpleStorage, bytecodeSimpleStorage, goerliSigner);
+
+const abiRLPReader = require('../../artifacts/contracts/RLPReader.sol/RLPReader.json').abi;
+const bytecodeRLPReader = require('../../artifacts/contracts/RLPReader.sol/RLPReader.json').bytecode;
+
+const RLPReader = new ethers.ContractFactory(abiRLPReader, bytecodeRLPReader, goerliSigner);
+
+const abiRelayer = require('../../artifacts/contracts/RelayContract.sol/RelayContract.json').abi;
+const bytecodeRelayer = require('../../artifacts/contracts/RelayContract.sol/RelayContract.json').bytecode;
+
+const Relay = new ethers.ContractFactory(abiRelayer, bytecodeRelayer, goerliSigner);
+
+async function deploySimpleStorage() {
+    logger.info('Deploying Simple Storage on Goerli');
+    try {
+        const SimpleStorageContract = await SimpleStorage.deploy();
+        await SimpleStorageContract.deployed();
+        logger.info(`Contract SimpleStorageContract deployed at: ${SimpleStorageContract.address}}`);
+    } catch (error) {
+        logger.error(error);
+    }
+}
+
+async function deployRLPReader() {
+    try {
+        logger.info('Deploying RLP Reader on Goerli');
+        const RLPReaderContract = await RLPReader.deploy();
+        await RLPReaderContract.deployed();
+        logger.info(`Contract RLPReaderContract deployed at: ${RLPReaderContract.address}`);
+    } catch (error) {
+        logger.error(error);
+    }
+}
+
+async function deployRelay() {
+    try {
+        logger.info('Deploying Relay on Goerli');
+        const RelayContract = await Relay.deploy();
+        await RelayContract.deployed();
+        logger.info(`Contract Relay deployed at: ${RelayContract.address}`);
+    } catch (error) {
+        logger.error(error);
+    }
+}
+
+async function main() {
+    // await deploySimpleStorage();
+    // await deployRLPReader();
+    await deployRelay();
+}
+
+main();
