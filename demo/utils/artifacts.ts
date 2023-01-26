7460728758c2b187b0612dc475750cd6087ce4ce
Rafael Belchior <rbelchior@blockdaemon.com>
2023-01-26 11:56:04 +0200

    chore: update contract artifacts
    Signed-off-by: Rafael Belchior <rbelchior@blockdaemon.com>

--- demo/utils/artifacts.ts
+++ demo/utils/artifacts.ts
@@ -1,54 +1,50 @@
 /* eslint-disable global-require */
-import { expect } from 'chai';
-import { Contract, ethers } from 'ethers';
-import { PROXY_INTERFACE } from '../../src/config';
+import { ethers } from 'ethers';
 import { logger } from '../../src/utils/logger';
-import ProxyContractBuilder from '../../src/utils/proxy-contract-builder';
-
 require('dotenv').config();
 
 logger.setSettings({ minLevel: 'info', name: 'demo-utils:deploy-contracts' });
 
 const {
-    PRIVATE_KEY, RPC_URL_GOERLI, RPC_URL_MUMBAI, DEPLOYED_CONTRACT_ADDRESS_STORAGE_MUMBAI, RPC_LOCALHOST_ORIGIN, RPC_LOCALHOST_TARGET, DEPLOYED_CONTRACT_ADDRESS_RELAY_GOERLI, DEPLOYED_CONTRACT_ADDRESS_SRC_CONTRACT_SYNC_CANDIDATE, DEPLOYED_CONTRACT_ADDRESS_LOGIC_CONTRACT_SYNC_CANDIDATE,
+    PRIVATE_KEY, RPC_URL_GOERLI, RPC_URL_ETH_MAINNET,RPC_URL_MUMBAI,
 } = process.env;
 
 export namespace ContractArtifacts {
 
     export const goerliProvider = new ethers.providers.JsonRpcProvider(RPC_URL_GOERLI);
-    export const polygonProvider = new ethers.providers.JsonRpcProvider(RPC_URL_MUMBAI);
+    export const ethereumProvider = new ethers.providers.JsonRpcProvider(RPC_URL_ETH_MAINNET);
+    export const mumbaiProvider = new ethers.providers.JsonRpcProvider(RPC_URL_MUMBAI);
 
     export const goerliSigner = new ethers.Wallet(PRIVATE_KEY as string, goerliProvider);
-    export const polygonSigner = new ethers.Wallet(PRIVATE_KEY as string, polygonProvider);
+    export const ethereumSigner = new ethers.Wallet(PRIVATE_KEY as string, ethereumProvider);
+    export const mumbaiSigner = new ethers.Wallet(PRIVATE_KEY as string, mumbaiProvider);
+
+    const sourceSigner = mumbaiSigner;
+    const targetSigner = goerliSigner;
 
     export const abiSimpleStorage = require('../../artifacts/contracts/SimpleStorage.sol/SimpleStorage.json').abi;
     export const bytecodeSimpleStorage = require('../../artifacts/contracts/SimpleStorage.sol/SimpleStorage.json').bytecode;
-
-    export const SimpleStorage = new ethers.ContractFactory(abiSimpleStorage, bytecodeSimpleStorage, goerliSigner);
+    export const SimpleStorageTarget = new ethers.ContractFactory(abiSimpleStorage, bytecodeSimpleStorage, targetSigner);
+    export const SimpleStorageSource = new ethers.ContractFactory(abiSimpleStorage, bytecodeSimpleStorage, sourceSigner);
 
     export const abiRLPReader = require('../../artifacts/contracts/RLPReader.sol/RLPReader.json').abi;
     export const bytecodeRLPReader = require('../../artifacts/contracts/RLPReader.sol/RLPReader.json').bytecode;
-
-    export const RLPReader = new ethers.ContractFactory(abiRLPReader, bytecodeRLPReader, goerliSigner);
+    export const RLPReaderTarget = new ethers.ContractFactory(abiRLPReader, bytecodeRLPReader, targetSigner);
 
     export const abiRelay = require('../../artifacts/contracts/RelayContract.sol/RelayContract.json').abi;
     export const bytecodeRelay = require('../../artifacts/contracts/RelayContract.sol/RelayContract.json').bytecode;
-
-    export const Relay = new ethers.ContractFactory(abiRelay, bytecodeRelay, goerliSigner);
+    export const RelayTarget = new ethers.ContractFactory(abiRelay, bytecodeRelay, targetSigner);
 
     export const abiMapping = require('../../artifacts/contracts/MappingContract.sol/MappingContract.json').abi;
     export const bytecodeMapping = require('../../artifacts/contracts/MappingContract.sol/MappingContract.json').bytecode;
-
-    export const Mapper = new ethers.ContractFactory(abiMapping, bytecodeMapping, goerliSigner);
+    export const MapperTarget = new ethers.ContractFactory(abiMapping, bytecodeMapping, targetSigner);
 
     export const abiSyncCandidate = require('../../artifacts/contracts/SyncCandidate.sol/SyncCandidate.json').abi;
     export const bytecodeSyncCandidate = require('../../artifacts/contracts/SyncCandidate.sol/SyncCandidate.json').bytecode;
-
-    export const SyncCandidate = new ethers.ContractFactory(abiSyncCandidate, bytecodeSyncCandidate, goerliSigner);
+    export const SyncCandidateTarget = new ethers.ContractFactory(abiSyncCandidate, bytecodeSyncCandidate, targetSigner);
 
     export const abiProxyContract = require('../../artifacts/contracts/ProxyContract.sol/ProxyContract.json').abi;
     export const bytecodeProxyContract = require('../../artifacts/contracts/ProxyContract.sol/ProxyContract.json').bytecode;
-
-    export const ProxyContract = new ethers.ContractFactory(abiProxyContract, bytecodeProxyContract, goerliSigner);
+    export const ProxyContractTarget = new ethers.ContractFactory(abiProxyContract, bytecodeProxyContract, targetSigner);
 
 }
