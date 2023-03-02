685bb462b9ac756de5506e2c6a3d8c3db277635e
Rafael Belchior <rbelchior@blockdaemon.com>
2023-03-02 10:12:22 +0000

    feat: public api

---
+++ src/public-api.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/export */
+export * from '../src-gen/types';
+
+export { TxContractInteractionOptions } from './cli/smart-sync';
+export * from './diffHandler/DiffHandler';
+export * from './utils/fileHandler';
+
+export {
+    ContractArtifacts,
+} from '../demo/utils/artifacts';
+
+export { TestChainProxySimpleStorage } from '../demo/utils/test-chain-proxy-simple-storage';
